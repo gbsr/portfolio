65adf93e1c5bb88be7b9788ae1397b4a6de8748a
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-01-09 20:12:48 +0100

    portfolio layout + styling

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+// this function resizes my hero container to always be below the header no matter how it is resized due to wrapping.
+window.addEventListener('resize', adjustContainerMargin);
+
+function adjustContainerMargin() {
+	let headerHeight = document.querySelector('header').offsetHeight;
+	document.querySelector('.container').style.marginTop = `${headerHeight}px`;
+}
+
+// Call the function initially to set the margin
+adjustContainerMargin();
