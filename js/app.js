bd2cac79670e5b49a7c51e9fbbfd350864c4978d
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-01-11 05:06:39 +0100

    header/footer observer/controller

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ document.querySelector('.turned').addEventListener('mouseleave', function (event
 		event.target.style.transform = 'rotate(0deg)';
 	}, 350); // delay in ms
 });
+
+let header = document.querySelector('header');
+let footer = document.querySelector('footer');
+
+let observer = new IntersectionObserver(function (entries) {
+	// entries[0] is the first (and only) entry in the array
+	if (!entries[0].isIntersecting) {
+		// The header is not visible, so show the footer
+		header.style.top = '-100vh';
+		footer.style.bottom = '0';
+	} else {
+		// The header is visible, so hide the footer
+		header.style.top = '0';
+		footer.style.bottom = '-100vh';
+	}
+});
+
+// Start observing the header
+observer.observe(header);
